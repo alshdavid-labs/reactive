43a92f0b57c4e07bd8f1e984663687441dc1a47d
David Alsh <alshdavid@gmail.com>
2020-11-19 17:20:41 +1100

    small check to make sure we don't re-init a watched object

--- src/reactive/observe-object.ts
+++ src/reactive/observe-object.ts
@@ -3,6 +3,9 @@ import { ProxyHandler } from './proxy-handler'
 import { createNodeId, KEY, state } from './state'
 
 export const proxyObjectInstance = <T>(source: T): T => {
+  if (typeof (source as any)[KEY] !== 'undefined') {
+    return source
+  }
   const nodeId = createNodeId()
   patchInstance(source, [nodeId])
   return new Proxy<any>(source, ProxyHandler())
